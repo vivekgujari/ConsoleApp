36fc5d46e0d17fba56bc7dfe5573b63f0012f3e7
Vivek Gujari <vivekgujari@gmail.com>
2023-02-06 21:35:40 -0700

    Added service class

---
+++ ConsoleApp/services/Crud.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Crud
+{
+	public Crud()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
